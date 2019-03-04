9784f7e9f62987b2516ec665e991f901c4c0856d
BorisB <butukhanovb@mail.ru>
2019-03-04 14:56:51 +0300

    add requirements

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import hashlib
 import os
+import wget
 
-def generate_file_md5(rootdir, filename, blocksize=2**20):
+def generate_file_md5(filename, blocksize=128):
     m = hashlib.md5()
-    with open(os.path.join(rootdir, filename), "rb") as f:
+    with open(os.path.join(filename), "rb") as f:
         while True:
             buf = f.read(blocksize)
             if not buf:
@@ -11,6 +12,21 @@ def generate_file_md5(rootdir, filename, blocksize=2**20):
             m.update(buf)
     return m.hexdigest()
 
+url = 'https://cloud.mail.ru/public/Fvwt/4wMN4P2Bc'
+work_name = "/home/borya/md5_hash_calculator/files/file"
+try:
+    if os.path.isfile(work_name):
+        os.unlink(work_name)
+    # elif os.path.isdir(file_path): shutil.rmtree(file_path)
+except Exception as e:
+    print(e)
 
-temp = generate_file_md5(rootdir='/home/borya/', filename='audio_test.mp3')
+try:
+    filename = wget.download(url)
+except:
+    print('error')
+
+os.rename("/home/borya/md5_hash_calculator/"+filename, work_name)
+temp = generate_file_md5(filename=work_name)
+
 print(temp)
