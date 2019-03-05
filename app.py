f25f65293e80dff1cda4f2e8fdf12c5c9035720a
BorisB <butukhanovb@mail.ru>
2019-03-05 18:27:08 +0300

    sent email

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 import hashlib
 import os
 import wget
+import smtplib
+def send_email(to_addr, body_text):
+    server = smtplib.SMTP('smtp.mail.ru', 25)
+    server.ehlo()
+    server.starttls()
+    server.login('test_boston_gene@mail.ru', 'boston2019')
+    server.sendmail('test_boston_gene@mail.ru', to_addr, msg=body_text)
+    server.quit()
 
-def md5(url = 'https://download.sublimetext.com/sublime_text_3_build_3176_x64.tar.bz2'):
-    def generate_file_md5(filename, blocksize=128):
-        m = hashlib.md5()
-        with open(os.path.join(filename), "rb") as f:
-            while True:
-                buf = f.read(blocksize)
-                if not buf:
-                    break
-                m.update(buf)
-        return m.hexdigest()
+def calculate_hash(filename, email, url, blocksize=128):
+    m = hashlib.md5()
+    with open(os.path.join(filename), "rb") as f:
+        while True:
+            buf = f.read(blocksize)
+            if not buf:
+                break
+            m.update(buf)
+    text_for_sent = 'adress file={}, calculated hash md5={}'.format(url, m.hexdigest())
+    send_email(to_addr=email, body_text=text_for_sent)
+    return m.hexdigest()
 
+def download_file(url = 'https://download.sublimetext.com/sublime_text_3_build_3176_x64.tar.bz2'):
     work_name = "/home/borya/md5_hash_calculator/files/file"
     try:
         if os.path.isfile(work_name):
@@ -22,8 +32,8 @@ def md5(url = 'https://download.sublimetext.com/sublime_text_3_build_3176_x64.ta
 
     try:
         filename = wget.download(url)
-    except:
+    except Exception as e:
         print('error')
-        os.rename("/home/borya/md5_hash_calculator/"+filename, work_name)
-    temp = generate_file_md5(filename=work_name)
-    print(temp)
+
+    os.rename("/home/borya/md5_hash_calculator/"+filename, work_name)
+    return work_name
--- fl.py
+++ fl.py
@@ -1,32 +1,47 @@
+import os
+from threading import Thread
 from flask import Flask
 from flask import jsonify, request
-from app import md5
+from app import calculate_hash, download_file
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Index Page'
-
-@app.route('/hello')
-def hello_world():
-    type('asd')
-    return 'Hello World!'
+status = {'id':None, 'download':None, 'md_5':None}
 
 @app.route('/submit', methods=['POST'])
-def asd():
-
+def md5_hash():
     if request.method == 'POST':
-        try:
-            url = request.form['url']
-            asd = md5(url=url)
-        except:
-            print('Error. website address is incorrect')
+        email = None
         try:
             email = request.form['email']
         except:
             print('No email')
+        try:
+            url = request.form['url']
+            file = download_file(url=url)
+            if os.path.isfile(file):
+                status['id'] = 1
+                status['download']=True
+                # md = calculate_hash(filename=file, url=url, email=email)
+                md = Thread(target=calculate_hash, args=(file, url, email))
+                variavle = md.start()
+                return variavle
+
+        except Exception as e:
+            print(e)
 
         print('excelent')
+
+@app.route('/check', methods=['GET'])
+def check():
+
+    if request.method == 'GET':
+        try:
+            id = request.form['id']
+        except:
+            print('Error. website address is incorrect')
+
+        print('excelent')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
